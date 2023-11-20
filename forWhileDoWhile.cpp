766e6b81a96d91a3375c1f5eb91aaa8bc1412d78
Emmanuelsaleemgit <emmanuelsaleem098765@gmail.com>
2023-11-21 01:08:03 +0500

    for while do loop

---
+++ forWhileDoWhile.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+    cout << "loop";
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     cout << "Emmanuel saleem" << endl;
+    // }
+
+    int i = 0;
+    // while (i < 10)
+    // {
+    //     cout << "Emmanuel saleem" << endl;
+    //     i = i + 1;
+    // }
+
+    do
+    {
+        cout << "Emmanuel saleem" << endl;
+        i = i + 1;
+    } while (i < 10);
+
+    return 0;
+}
